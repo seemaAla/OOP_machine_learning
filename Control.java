9a95851ba95d92b2ffe1ab20452df99654ed9ef3
seemaAla <158099275+seemaAla@users.noreply.github.com>
2025-04-30 02:05:54 +0100

    Create Control.java

---
+++ Control.java
@@ -0,0 +1,13 @@
+
+public class Control 
+{
+	public static void main(String[] args) 
+	{
+        PersonIsVaccinated person1 = new PersonIsVaccinated();
+        GUI gui = new GUI(person1);
+        
+	}
+
+}
+
+
